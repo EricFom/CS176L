e8fb2a3942e51fc1bcabfcf473afadedc6d1f0e4
EricFom <144383023+EricFom@users.noreply.github.com>
2023-09-07 16:08:22 -0400

    my first java program version 1

---
+++ hello.java
@@ -0,0 +1,14 @@
+package helloWorld;
+
+public class hello 
+{
+
+	public static void main(String[] args) 
+	{
+		//CS176L
+		//Eric Fomichev
+		//helloWorld
+		System.out.println("Hello World");
+	}
+
+}
