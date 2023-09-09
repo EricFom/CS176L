0d8fcabc84cd11883657dbf6e4e9a3fb5e2569fc
EricFom <144383023+EricFom@users.noreply.github.com>
2023-09-09 08:19:07 -0400

    my first java project version 2

--- hello.java
+++ hello.java
@@ -5,10 +5,15 @@ public class hello
 
 	public static void main(String[] args) 
 	{
-		//CS176L
-		//Eric Fomichev
-		//helloWorld
-		System.out.println("Hello World");
+		/*
+		 * CS176L
+		 * Eric Fomichev
+		 * helloWorld
+		 */
+		
+		
+		System.out.println("Hello World this is Eric Fomichev");
+
 	}
 
 }
